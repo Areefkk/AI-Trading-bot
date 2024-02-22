447fbd3ea881ac9bfc4c8a1d07362a118af766ce
Areef Khan <areef_khan@outlook.com>
2024-02-22 20:20:21 +0000

    API generated via Alpaca

--- tradingbot.py
+++ tradingbot.py
@@ -3,3 +3,16 @@ from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime 
+
+# Generated via Alpaca
+API_KEY = "PKNBRV0GEQP7Q5OQXBUQ" 
+API_SECRET = "TBz6Mjl7hNrZ59b7x2N55Qljqr6THKE12gf09uEu"
+BASE_URL = "https://paper-api.alpaca.markets"
+
+ALPACA_CREDS = {
+    "API_KEY" : API_KEY,
+    "API_SECRET" : API_KEY,
+    "PAPER" : True
+}
+
+
