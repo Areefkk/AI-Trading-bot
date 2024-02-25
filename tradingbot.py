2d7753991000c81f539cd4ede25c9d8ab151b445
Areef Khan <areef_khan@outlook.com>
2024-02-25 14:53:01 +0000

    Appropriate cash management strategies set

--- tradingbot.py
+++ tradingbot.py
@@ -16,28 +16,40 @@ ALPACA_CREDS = {
 }
 
 class MLTrader(Strategy):
-    def initialise(self, symbol:str="SPY"):  ## This initialise method will runs once, when the bot starts up
+    def initialise(self, symbol:str="SPY", cash_at_risk:float):  ## This initialise method will runs once, when the bot starts up
         self.symbol = symbol
         self.sleeptime = "24hr"  ## Dictates how frequently the bot will trade
         self.last_trade = None   ## This captures our last trade, incase we need to undo our buys/sells
+        self.cash_at_risk = cash_at_risk
+
+    def position_sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+        quantity = cash * self.cash_At_risk / last_price
+        return cash, last_price, quantity
         
     def on_trading_iteration(self):  ## This iteration will execute everytime new data is retrieved from the data source
-        if self.last_trade == None:
-            order = self.create_order(
-                self.symbol,
-                10,
-                "buy",
-                type="market"
-            )
-            self.submit_order(order)
-            self.last_trade = "buy"
-        # return super().on_trading_iteration()
+        cash, last_price, quantity = self.position_sizing()
+
+        if cash > last_price: # We're not just buying when we don't have cash
+          if self.last_trade == None:
+              order = self.create_order(
+                 self.symbol,
+                 quantity,
+                 "buy",
+                 type="bracket"
+                 take_profit_price= last_price*1.20, 
+                 stop_loss_price-last_price*.95 
+                 )
+             self.submit_order(order)
+             self.last_trade = "buy"
 
 start_date = datetime(2023,12,15)    
 end_date = datetime(2023,12,30)  
 broker = Alpaca(ALPACA_CREDS)
 strategy = MLTrader(name='mlstrat', broker=broker,
-                    parameters={"symbol":"SPY"}) 
+                    parameters={"symbol":"SPY", 
+                                "cash_at_risk":.5}) 
 strategy.backtest(
     YahooDataBacktesting,
     start_date,
